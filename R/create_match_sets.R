cb63838faf8c850472740bdb6a480b9f31305015
MHelmann <maksim.helmann@gmail.com>
2024-11-21 21:49:01 -0500

    Write creat_match_sets and comment code

---
+++ R/create_match_sets.R
@@ -0,0 +1,66 @@
+#' @title Create Matched Sets Using Pair Matching
+#' @description This function creates matched sets of treated and control individuals using pair matching based on a risk score. It computes the mean risk score and treatment effect for each matched set.
+#' @param treated A binary vector indicating treatment status (1 for treated, 0 for control).
+#' @param risk_scores A numeric vector of risk scores for all individuals.
+#' @param control_treated_ratio An integer specifying the ratio of controls to treated individuals for matching.
+#' @param response A numeric vector of response values for the individuals.
+#' @return A matrix where each row corresponds to a matched set. The columns include:
+#' - IDs of controls and treated individuals in each matched set.
+#' - Risk scores for controls and treated individuals.
+#' - The mean risk score of the matched set.
+#' - The treatment effect for the matched set.
+#' The rows are ordered by the mean risk score of the matched sets.
+#' @importFrom optmatch pairmatch
+#' @author Danny Del Rosso, Maksim Helmann
+#' @export
+
+creat_match_sets <- function(treated, risk_scores, control_treated_ratio, response){
+
+  df <- data.frame(trt=treated, score=risk_scores, idx=1:length(treated))
+
+  # Perform pair matching using the 'pairmatch()' function:
+  # - Matches individuals based on their treatment status ('trt') and risk scores ('score').
+  # - 'controls' specifies the ratio of controls to treated individuals for matching.
+  pm <- pairmatch(trt ~ score, data=df, controls=control_treated_ratio, remove_unmatchables = TRUE)
+  # Remove any NA values from the matching results to keep only successfully matched pairs.
+  pm <- pm[!is.na(pm)]
+
+
+  # Compute information for each match (mean risk score, treatment effect)
+  get_group_info <- function(group){
+
+    # Extract indices of the group members
+    individual_idx <- as.numeric(names(pm[pm == group]))
+    # Index of controls (individuals in the group who are not treated)
+    ctrls <- individual_idx[individual_idx %in% df[df$trt == 0, "idx"]]
+    # Index of treated individuals (individuals in the group who are treated)
+    trtds <- individual_idx[individual_idx %in% df[df$trt == 1, "idx"]]
+
+    # Compute treatment effect
+    treatment_effect <- mean(response[trtds] - response[ctrls])
+
+    c(ctrls, trtds,                                    # id of each control/treated
+      risk_scores[ctrls],                              # risk scores for controls
+      risk_scores[trtds],                              # risk scores for treated
+      mean(c(risk_scores[ctrls], risk_scores[trtds])), # mean score for the group
+      treatment_effect                                 # treatment effect
+    )
+  }
+
+  # Extract matching information for all unique matched groups:
+  # - 'unique(pm)' gets all unique match identifiers from 'pm'.
+  # - 'sapply(unique(pm), get_group_info)' applies the 'get_group_info' function to each unique match.
+  # - 't()' transposes the result to arrange it in a tabular format.
+  all.matches <- t(sapply(unique(pm), get_group_info))
+
+  # Create column names for the matched groups:
+  # - If the control-to-treated ratio is 1, use "control" as the column name.
+  # - Otherwise, create separate names for each control (e.g., "control_1", "control_2", etc.).
+  ctrl.names <- if(control_treated_ratio == 1) {"control"} else {paste0("control_", 1:control_treated_ratio)}
+
+  colnames(all.matches) <- c(ctrl.names, "treated", paste0(ctrl.names, "_score"), "treated_score", "mean_score", "treatment_effect")
+
+  # return the matches in order of increasing risk score
+  all.matches[order(all.matches[, "mean_score"]), ]
+
+}
